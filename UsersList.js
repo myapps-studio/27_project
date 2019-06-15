60a06d10f8d08bdcb206cc24919b2638b9fbd102
Andrzej Mieczkowski <praca.mieczkowski@gmail.com>
2019-06-15 09:00:42 +0200

    first commit

---
+++ UsersList.js
@@ -0,0 +1,17 @@
+import User from './User';
+
+class UsersList extends React.Component {
+    get users() {
+      return this.props.users.map(user => <User key={user.id} user={user}/>);
+    }
+  
+    render() {
+      return (
+        <div>
+          {this.users}
+        </div>
+      );
+    }
+  }
+
+  
